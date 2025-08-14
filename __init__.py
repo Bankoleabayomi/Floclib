0a8466e16b95cdfcdc0a79da0b4fdecd84f87b7e
Bankoleabayomi <bankoleabayomi3@gmail.com>
2025-08-13 22:56:13 -0300

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 13 13:39:05 2025
+
+@author: banko
+"""
+
+# floclab/__init__.py
+"""floclab package: core access points."""
+
+# expose core functions at package-level if you like
+from .asd import compute_beta_from_features
+from .fit import fit_ka_kb
+from .cstr import simulate_retention_times
+
+__all__ = ["compute_beta_from_features", "fit_ka_kb", "simulate_retention_times"]
