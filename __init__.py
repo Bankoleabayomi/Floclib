499f45b94f8af395f34aeaf1a846eb359d4334d7
Bankoleabayomi <bankoleabayomi3@gmail.com>
2025-08-14 01:08:26 -0300

    Versioning

--- __init__.py
+++ __init__.py
@@ -8,6 +8,8 @@ Created on Wed Aug 13 13:39:05 2025
 # floclab/__init__.py
 """floclab package: core access points."""
 
+__version__ = "0.1.0"   # match pyproject.toml version
+
 # expose core functions at package-level if you like
 from .asd import compute_beta_from_features
 from .fit import fit_ka_kb
